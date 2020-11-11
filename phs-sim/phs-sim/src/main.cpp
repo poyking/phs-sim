50c4beef32471b2160822a7485075205ad9a3203
poyking <poyrazauer@gmail.com>
2020-11-11 11:48:35 +0000

    finish steam sim

--- phs-sim/phs-sim/src/main.cpp
+++ phs-sim/phs-sim/src/main.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
 #include <time.h>
 #include <chrono>
-#include <ctime>  
+#include <ctime>
+#include <glm/vec3.hpp>
+#include <glm/ext.hpp>
+#include <glm/gtx/matrix_transform_2d.hpp>
 float A = 90;
-float M = 5;
+float M = 10;
 float COF = 1.6;
 float FOF;
-float F = 100;
+float F = 400;
 float D = 2;
 float r = 0;
-float dt = 1;
+long double dt = 0.01;
 
+glm::vec3 p1 = glm::vec3(1);
+glm::vec3 p2 = glm::vec3(1);
 int main()
 {
 	while (1)
 	{
-		auto L = std::chrono::high_resolution_clock::now();
 		FOF = COF * (M * 9.8);
 		float CF = F - FOF;
 		float AV = (F * D * sinf(A)) / (M * (D * D));
-		r += AV * 0.00001;
-		std::cout << r << std::endl;
-		auto N = std::chrono::high_resolution_clock::now();
-		float dt = (N - L)
+		r += AV * dt;
+		glm::mat3 TP1 = glm::mat3(1);
+		TP1 = glm::rotate(TP1, r);
+		TP1 = glm::translate(TP1, glm::vec2(200, 0));
+		p1 = TP1 * p1;
+
+		p2 = glm::vec3(200, 0, 0);
+
+		std::cout << "wheel rotation: " << r << std::endl;
+		std::cout << "axel p1: " << p1.x << ", " << p1.y << std::endl;
+		std::cout << "axel p2: " << p2.x << ", " << p2.y << std::endl;
+		system("cls");
 	}
 }
